51b39e32bb2acd6a633af89d095476390db081f7
Veronica Day <veronica@trove.com>
2018-11-08 21:33:24 -0500

    made journal frag

--- AreteApp/app/src/main/java/com/example/veronica/areteapp/HomeFragment.java
+++ AreteApp/app/src/main/java/com/example/veronica/areteapp/HomeFragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -17,7 +19,7 @@ import android.widget.TextView;
 public class HomeFragment extends Fragment
 {
 
-    private TextView textGreeting;
+    private EditText editGreeting;
     private EditText editTextAnswer;
     private ImageButton buttonEdit;
 
@@ -32,14 +34,43 @@ public class HomeFragment extends Fragment
                              Bundle savedInstanceState)
     {
         View rootview = inflater.inflate(R.layout.fragment_home, container, false);
-        textGreeting = (TextView)rootview.findViewById(R.id.textGreeting);
+        editGreeting = (EditText) rootview.findViewById(R.id.editGreeting);
         editTextAnswer = (EditText) rootview.findViewById(R.id.editTextAnswer);
         buttonEdit = (ImageButton) rootview.findViewById(R.id.buttonEdit);
+
+        setGreeting();
 
         // Inflate the layout for this fragment
         return rootview;
     }
 
+    private void setGreeting()
+    {
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        String greeting = "";
+
+        if(timeOfDay >= 0 && timeOfDay < 12)
+        {
+            greeting = "Good Morning!";
+            editGreeting.setText(greeting);
+        }
+        else if(timeOfDay >= 12 && timeOfDay < 16)
+        {
+            greeting = "Good Afternoon!";
+            editGreeting.setText(greeting);
+        }
+        else if(timeOfDay >= 16 && timeOfDay < 21)
+        {
+            greeting = "Good Evening!";
+            editGreeting.setText(greeting);
+        }
+        else if(timeOfDay >= 21 && timeOfDay < 24)
+        {
+            greeting = "Good Night!";
+            editGreeting.setText(greeting);
+        }
+    }
 }
 
 
--- AreteApp/app/src/main/java/com/example/veronica/areteapp/JournalFragment.java
+++ AreteApp/app/src/main/java/com/example/veronica/areteapp/JournalFragment.java
@@ -6,13 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class JournalFragment extends Fragment {
-
+public class JournalFragment extends Fragment
+{
+    private EditText editDate;
+    ArrayAdapter<String> mTaskAdapter;
 
     public JournalFragment() {
         // Required empty public constructor
@@ -21,9 +33,42 @@ public class JournalFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
+        View rootview = inflater.inflate(R.layout.fragment_journal, container, false);
+
+        editDate = rootview.findViewById(R.id.editDate);
+        setDate();
+
+        //Create the fake data
+        String[] fakeData = {
+                "Revise for exam",
+                "Buy milk",
+                "Do laundry",
+                "Call Melissa",
+                "Buy stamps",
+        };
+        List<String> tasks = new ArrayList<String>(Arrays.asList(fakeData));
+
+        //Create the ArrayAdapter by specifying context, "how" (layout),"where" (textview), and " what" (data)
+        mTaskAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_task,
+                R.id.list_item_task_textview,
+                tasks);
+
+        //Still need to bind adapter to the ListView
+        ListView listView = (ListView) rootview.findViewById(R.id.listViewTask);
+        listView.setAdapter(mTaskAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_journal, container, false);
+        return rootview;
+
     }
 
+    private void setDate()
+    {
+        Calendar cal=Calendar.getInstance();
+        editDate.setText(android.text.format.DateFormat.format("MMMM dd, yyyy", new java.util.Date()));
+    }
 }
