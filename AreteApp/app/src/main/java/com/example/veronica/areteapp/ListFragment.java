ffef327cb84d1a771f33f50358335a57d57ac0ba
Veronica Day <veronica@trove.com>
2018-11-08 22:35:54 -0500

    update gradle

--- AreteApp/app/src/main/java/com/example/veronica/areteapp/ListFragment.java
+++ AreteApp/app/src/main/java/com/example/veronica/areteapp/ListFragment.java
@@ -11,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
